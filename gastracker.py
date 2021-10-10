c4de9f20db9914d130af1d375d03cf15e41c0dee
Josh Münte <josh.muente@e.email>
2021-10-10 17:23:25 +0200

    init

---
+++ gastracker.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+import re
+import time
+import sys
+
+class Tracker:
+    def __init__(self):
+        # fake headers to bypass cloudflare security
+        self.parseArgs()
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        self.session = requests.Session()
+        self.getSid()
+        self.loop()
+
+    def parseArgs(self):
+        if len(sys.argv) < 3:
+            self.low = 50
+            self.medium = 100
+        else:
+            self.low = int(sys.argv[1])
+            self.medium = int(sys.argv[2])
+
+    def getSid(self):
+        req = self.session.get('https://etherscan.io/gastracker', headers=self.headers)
+        regex = r"var sid = '(.*)';"
+        api_sid = re.findall(regex, req.text)[0]
+        self.sid = api_sid
+
+    def loop(self):
+        while True:
+            req = self.session.get('https://etherscan.io/autoUpdateGasTracker.ashx?sid=' + self.sid, headers=self.headers)
+            status_code = req.status_code
+            if status_code != 200:
+                self.getSid()
+                continue
+            data = req.json()
+            self.data = data;
+            self.output()
+            time.sleep(5)
+
+    def output(self):
+        text = "low: " + self.data['lowPrice'] + ' avg: ' + self.data['avgPrice'] + ' high: ' + self.data['highPrice']
+        css_class = "low"
+        if int(self.data['avgPrice']) > self.medium:
+            css_class = "high"
+        elif int(self.data['avgPrice']) > self.low:
+            css_class = "medium"
+        out = {'text': text, 'class': css_class}
+        print(json.dumps(out), flush=True)
+
+if __name__ == "__main__":
+    tracker = Tracker()
